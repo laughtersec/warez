1856ba314ce679c2ce074e17f349648d38ed00d9
laughtersec <178651847+laughtersec@users.noreply.github.com>
2026-03-31 12:42:21 +0530

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/laughtersec",
+      "Malectrica Discord": "https://discord.gg/jByxKBmJ9T",
     },
   }),
 }
